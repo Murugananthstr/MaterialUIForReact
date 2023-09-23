81aebbf401a390eb01ce18c448747bc5d4a88c87
Ananth <ananthsoft@gmail.com>
2023-09-23 02:23:13 -0700

    Typography color variant

--- react-mui-demo/src/components/MuiButton.tsx
+++ react-mui-demo/src/components/MuiButton.tsx
@@ -3,10 +3,36 @@ import React from "react";
 
 const MuiButton = () => {
   return (
-    <Stack spacing={2} direction="column">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+    <Stack spacing={4} alignItems={"center"}>
+      <Stack spacing={2} direction="column">
+        <Button variant="text" color="secondary" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined" color="warning">
+          Outlined
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="warning">
+          warning
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
     </Stack>
   );
 };
